c619d4e03dd17c2e4330785d56992fedbb22212f
火武凌风 <China-xu@users.noreply.github.com>
2019-03-01 16:09:55 +0800

    self.title

--- JY/JY/ViewController.swift
+++ JY/JY/ViewController.swift
@@ -12,7 +12,11 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.title = "测试"
+        add()
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    func add() {
     }
 
 

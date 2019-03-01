16eb35d333ad8d9ecf50e4ceb752cd5fc968c105
火武凌风 <China-xu@users.noreply.github.com>
2019-03-01 14:50:21 +0800

    diyic

---
+++ JY/JY/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  JY
+//
+//  Created by 徐庆标 on 2019/3/1.
+//  Copyright © 2019 徐庆标. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+

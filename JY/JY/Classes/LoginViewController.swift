3432eaf0213cffd9901b6dca11c8eb9be8e3b0c9
文 <363536404@qq.com>
2019-03-01 16:40:36 +0800

    登录

---
+++ JY/JY/Classes/LoginViewController.swift
@@ -0,0 +1,30 @@
+//
+//  LoginViewController.swift
+//  JY
+//
+//  Created by 华通众和 on 2019/3/1.
+//  Copyright © 2019年 徐庆标. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ JY/JY/TestViewController.swift
@@ -0,0 +1,30 @@
+//
+//  TestViewController.swift
+//  JY
+//
+//  Created by 徐庆标 on 2019/3/1.
+//  Copyright © 2019 徐庆标. All rights reserved.
+//
+
+import UIKit
+
+class TestViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

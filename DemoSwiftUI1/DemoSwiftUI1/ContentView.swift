6296fb3244f77fd789b8046ee0a2eb50a08b5599
sun yao <316625387@qq.com>
2022-07-06 14:38:59 +0800

    根据Apple 教程 编写的 SwiftUI - 第一次写

---
+++ DemoSwiftUI1/DemoSwiftUI1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DemoSwiftUI1
+//
+//  Created by study on 2022/7/6.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DemoSwiftUI1/DemoSwiftUI1/DemoSwiftUI1App.swift
@@ -0,0 +1,17 @@
+//
+//  DemoSwiftUI1App.swift
+//  DemoSwiftUI1
+//
+//  Created by study on 2022/7/6.
+//
+
+import SwiftUI
+
+@main
+struct DemoSwiftUI1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

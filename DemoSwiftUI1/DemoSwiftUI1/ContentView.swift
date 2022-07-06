4830dce75f2328908e99509c96719c98c5f41f52
sun yao <316625387@qq.com>
2022-07-06 14:49:08 +0800

    创建和组合视图：1. 创建一个新项目并了解画布

--- DemoSwiftUI1/DemoSwiftUI1/ContentView.swift
+++ DemoSwiftUI1/DemoSwiftUI1/ContentView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
+        Text("Hello, SwiftUI!")
             .padding()
     }
 }
@@ -19,3 +19,15 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+/**
+ 一个 SwiftUI 视图文件 会有2个结构。
+ 1. 第一个结构 是编写视图的内容和布局的。
+ 2. 第二个结构是 声明这个视图的预览。
+ 
+ 画布：
+ 在右侧，有一个画布，这个画布会显示 UI 界面。
+ 如果没有显示这个画布，点击 编辑器 -> 画布 来显示它。
+ 当你修改当前页面的UI时，画布会自动更新。
+ 
+ */
--- DemoSwiftUI1/DemoSwiftUI1/DemoSwiftUI1App.swift
+++ DemoSwiftUI1/DemoSwiftUI1/DemoSwiftUI1App.swift
@@ -15,3 +15,10 @@ struct DemoSwiftUI1App: App {
         }
     }
 }
+
+/**
+ 这是应用程序的入口点
+ 在 Xcode 13 及 以上的版本中，类名是 LandmarksApp
+ swiftUI 的应用程序生命周期，应该继承至 App .
+ body 属性会返回一个或多个场景。这些场景会提供显示内容。
+ */
